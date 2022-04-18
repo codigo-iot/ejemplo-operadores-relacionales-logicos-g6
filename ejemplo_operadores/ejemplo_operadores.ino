ebb005d5b18fa11294a31b5d3d2e70778a0428fb
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-04-18 17:26:02 -0500

    Encabezado

--- ejemplo_operadores/ejemplo_operadores.ino
+++ ejemplo_operadores/ejemplo_operadores.ino
@@ -1,9 +1,32 @@
+
 /*
- * Estructura básica
+ * Simulador de refrigerasion de servidores
  * Por: Hugo Vargas
- * Fecha: 28 de junio de 2021
+ * Fecha: 18 de abril 2022 (32°C)
  * 
- * Esto es una muestra de la estructura básica de un programa
+ * Este programa ejemplifica el uso de operadores relacionales y operadores
+ * logicos:
+ * 
+ * Condiciones Iniciales
+ * Sensor de temperatura y humedad
+ * Boton1: Manual. Activa la refrigeración al ser presionado
+ * Boton2: Alta demanda
+ * Boton3: Sobre carga de funcionamiento
+ * 
+ * Led1: Refrigeracion manual
+ * Led2: Refrigeración automática
+ * 
+ * -Que se encienda la refrigeracion manual cada que se presione el boton de manual
+ * -La refrigeracion manual tiene prioridad a la refrigeracion automática.
+ * -Si la termperatura es alta (mayor a 28, por ejemplo), se activa la refrigeracion automática
+ * -Si tengo alta demanda o sobre carga, se activa la refrigeración automática
+ * -Si tengo alta demanda o sobrecarga de funcionamiento y ademas temperatura alta, se activan ambas refrigeraciones
+ * 
+ * Conexiones propuestas
+ * Botones 14, 15, 13
+ * Leds 4, 2
+ * DHT11 12
+ * 
  */
 
 // Bibliotecas
